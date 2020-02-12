76330a8a5614c1d9c1d5dcdf56c541118e319543
root <root@localhost.localdomain>
2020-02-12 07:18:58 +0300

    created a new project and initialized the app

---
+++ app/src/main/java/inc/smart/solutions/imayaprofile/MainActivity.java
@@ -0,0 +1,14 @@
+package inc.smart.solutions.imayaprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
