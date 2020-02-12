59ba8aba31d8f0f7a2b808a253bb221b9b52a2b6
root <root@localhost.localdomain>
2020-02-12 15:16:21 +0300

    styled the profile and implemented a grid view place holder

---
+++ app/src/main/java/inc/smart/solutions/imayaprofile/models/Beanclass.java
@@ -0,0 +1,19 @@
+package inc.smart.solutions.imayaprofile.models;
+
+public class Beanclass {
+
+    private int image;
+
+    public Beanclass(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
+
