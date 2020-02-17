76f70f0d788c05a8b2607328c7c88906eaa5d5b9
root <root@localhost.localdomain>
2020-02-17 07:37:01 +0300

    added the retrofit lib and its implementation

---
+++ app/src/main/java/inc/smart/solutions/imayaprofile/constants/Configs.java
@@ -0,0 +1,9 @@
+package inc.smart.solutions.imayaprofile.constants;
+
+public class Configs {
+    public static final String BASE_URL = "https://api.github.com/";
+
+    public static final class GitHubEndPoints {
+        public static final String USER = "users/ImayaDismas";
+    }
+}
