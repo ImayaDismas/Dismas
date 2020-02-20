7e152a5c59fa6b3e0f1d542b12a7b25d7685c07e
ImayaDismas <imayadismas@gmail.com>
2020-02-20 10:12:34 +0300

    added a typewriter textview and font for the slogan

--- app/src/main/java/inc/smart/solutions/imayaprofile/SplashActivity.java
+++ app/src/main/java/inc/smart/solutions/imayaprofile/SplashActivity.java
@@ -1,9 +1,18 @@
 package inc.smart.solutions.imayaprofile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import inc.smart.solutions.imayaprofile.utils.Typewriter;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,11 +21,15 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        Typewriter tvSlogan = findViewById(R.id.tvSlogan);
+        tvSlogan.setCharacterDelay(50);
+        tvSlogan.animateText(getResources().getString(R.string.slogan));
+
         Thread background = new Thread() {
             public void run() {
 
                 try {
-                    sleep(2*1000);
+                    sleep(getResources().getString(R.string.slogan).length() * 50 + 1000);
                     startActivity(new Intent(SplashActivity.this, MainActivity.class));
                     finish();
 
