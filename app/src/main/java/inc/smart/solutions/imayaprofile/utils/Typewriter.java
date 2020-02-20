9a1c9b0e5acdc84358824e6ec56f31076eaad957
ImayaDismas <imayadismas@gmail.com>
2020-02-20 10:12:34 +0300

    added a typewriter textview and font for the slogan

---
+++ app/src/main/java/inc/smart/solutions/imayaprofile/utils/Typewriter.java
@@ -0,0 +1,46 @@
+package inc.smart.solutions.imayaprofile.utils;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class Typewriter extends AppCompatTextView {
+
+    private CharSequence mText;
+    private int mIndex;
+    private long mDelay = 500; //Default 500ms delay
+
+
+    public Typewriter(Context context) {
+        super(context);
+    }
+
+    public Typewriter(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    private Handler mHandler = new Handler();
+    private Runnable characterAdder = new Runnable() {
+        @Override
+        public void run() {
+            setText(mText.subSequence(0, mIndex++));
+            if(mIndex <= mText.length()) {
+                mHandler.postDelayed(characterAdder, mDelay);
+            }
+        }
+    };
+
+    public void animateText(CharSequence text) {
+        mText = text;
+        mIndex = 0;
+        setText("");
+        mHandler.removeCallbacks(characterAdder);
+        mHandler.postDelayed(characterAdder, mDelay);
+    }
+
+    public void setCharacterDelay(long millis) {
+        mDelay = millis;
+    }
+}
